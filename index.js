decc79a47e16b5031c26a038f366ca1e8fe525ca
Sarah Baughman <sarahbaughman92@gmail.com>
2023-03-19 13:32:53 -0400

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1*num2
+
+const random = Math.floor(Math.random()*10 +1)
+
+const num3 = 14
+const num4 = 5
+const mod = num3 % num4
+
+const max =(2,4,6,8,20)
+Math.max
